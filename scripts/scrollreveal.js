48cfcc033e4744c63f8971ee6e6283c562afa857
DimitryGonzales <dimitrygonzales@gmail.com>
2025-08-10 23:36:16 -0300

    Add footer & language selection

--- scripts/scrollreveal.js
+++ scripts/scrollreveal.js
@@ -1,9 +1,10 @@
-// Left
-ScrollReveal().reveal('label[for="navbar-toggle"]', {
+// Top
+ScrollReveal().reveal('.footer-section', {
     duration: 500,
     distance: '50px',
     easing: 'ease',
-    origin: 'left',
+    interval: 100,
+    origin: 'top',
     reset: false
 });
 
@@ -12,6 +13,7 @@ ScrollReveal().reveal('.language-selector-container, .return-to-top', {
     duration: 500,
     distance: '50px',
     easing: 'ease',
+    interval: 100,
     origin: 'right',
     reset: false
 });
@@ -23,5 +25,15 @@ ScrollReveal().reveal('#hero, .hero-image-container, .hero-text, .hero-shortcut,
     easing: 'ease',
     interval: 100,
     origin: 'bottom',
+    reset: false
+});
+
+// Left
+ScrollReveal().reveal('label[for="navbar-toggle"]', {
+    duration: 500,
+    distance: '50px',
+    easing: 'ease',
+    interval: 100,
+    origin: 'left',
     reset: false
 });
